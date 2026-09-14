5efe5d163ffcdc0de003112f2605f6d1d3c09a7b
Joey Grable <hello@joeygrable.com>
2026-09-14 10:27:30 -0700

    removed prettier config dep

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,12 @@
-import gcPrettierTailwindApp from "@getcommunity/config-prettier/app-tailwind.js"
-
-export default gcPrettierTailwindApp
+export default {
+  $schema: "http://json.schemastore.org/prettier",
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 88,
+  trailingComma: "none",
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"]
+}
